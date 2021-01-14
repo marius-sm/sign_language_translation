c0e4da81bd877a798ebf6080f8ae857441bb9e40
clems grisi <clementnet@outlook.com>
2021-01-14 20:39:46 +0100

    added plug-in dictionnary with DOPE embeddings

---
+++ dope/data/dev_embedding_dict.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wget https://drive.google.com/uc?id=1-3l-6FK5QwcbLc9Zs0JGn0H6tEBX-vrG&export=download
---
+++ dope/data/train_embedding_dict.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wget https://drive.google.com/uc?id=18XaEvLTZSwkV2nWr-U4oAm_Kh_eUO7UV&export=download
