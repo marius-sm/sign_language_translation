b4b5b760c89175d2ddb87cf2e8369b14d90dada6
clems grisi <clementnet@outlook.com>
2021-01-14 17:12:58 +0100

    added download links for dope embeddings and keypoints

---
+++ data/train_keypoints.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+curl -OL https://drive.google.com/uc?id=12MniEkUWmecTlIY4-z2ry50BboyUE5Hn&export=download
+curl -OL https://drive.google.com/uc?id=1IOJsy7KWiz8VVaGiTwN1aQvIWDBqYd0e&export=download
+curl -OL https://drive.google.com/uc?id=1ujZSMMYEBWG89LwH-KUTMy1zU7e1s5ZU&export=download
+curl -OL https://drive.google.com/uc?id=1-1x7TaCdBsWQ7OQIXeGtaP-voR-H_xB7&export=download
+curl -OL https://drive.google.com/uc?id=1d3zCqIIxDckvNHmzLZDFkvpYLjTrRDnj&export=download
+curl -OL https://drive.google.com/uc?id=1cPRYowihAWJuB-OtFHCCW0l9CSvjBe8q&export=download
