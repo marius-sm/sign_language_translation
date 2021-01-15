7eecd78c59566e1cdf5361b2e81b24425b4d3dbd
clems grisi <clementnet@outlook.com>
2021-01-15 02:30:58 +0100

    had to split train embedding pickle object in 3 sub objects for RAM purposes

--- dope/data/train_embedding_dict.sh
+++ dope/data/train_embedding_dict.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 gdown https://drive.google.com/uc?id=18XaEvLTZSwkV2nWr-U4oAm_Kh_eUO7UV&export=download
+gdown https://drive.google.com/uc?id=1FyHvYziRo5QGp0QUXyJIzPCxUDFMhU6o&export=download
+gdown https://drive.google.com/uc?id=1b3UUOzHF6RmvQ8_nQRqJrzmTMvzQGBPi&export=download
---
+++ dope/data/train_keypoint_dict.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+gdown 
+gdown 
+gdown 
