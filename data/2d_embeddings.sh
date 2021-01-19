02b2a1b84b2bca7523a2b7d2c2c2d441ffd0e486
clementgr <jupyter@packer-5feabc72-7e50-797b-8549-cf82f531e6d0.c.deeplearning-platform.internal>
2021-01-19 23:07:43 +0000

    gcp instance modifications

---
+++ data/2d_embeddings.sh
@@ -0,0 +1,4 @@
+
+#!/usr/bin/env bash
+wget --directory-prefix=sign_language_translation/data/embeddings/ "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.train"
+wget --directory-prefix=sign_language_translation/data/embeddings/ "http://cihancamgoz.com/files/cvpr2020/phoenix14t.pami0.dev"
