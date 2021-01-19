ac73f1708b96fc3bf552531d3e3745d33d3d2cc9
clems grisi <clementnet@outlook.com>
2021-01-20 00:14:06 +0100

    removed old links to splitted version of the embeddings

--- dope/data/train_embedding_avg_dict.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-gdown "https://drive.google.com/uc?id=1-87322LCYb1kv7LgKgLB62rONnXr0A2N&export=download" --output sign_language_translation/dope/data/embeddings/
-gdown "https://drive.google.com/uc?id=1W0yF_T6-1O33mOVivXJDoe7YQ3aw4HQM&export=download" --output sign_language_translation/dope/data/embeddings/
-gdown "https://drive.google.com/uc?id=1-5V9gPxq0B7m6eGjCRts3i8TJF8tXOts&export=download" --output sign_language_translation/dope/data/embeddings/
